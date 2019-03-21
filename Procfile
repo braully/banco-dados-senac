2c3b650e248f8dcb00ddf3806786d205155d16c6
strike <strike@alastor-note>
2019-03-21 18:50:13 -0300

    aulas

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/banco-dados-senac-1.0.jar
---
+++ src/main/java/com/github/braully/bds/web/AplicacaoWeb.java
@@ -0,0 +1,17 @@
+package com.github.braully.bds.web;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ *
+ * @author braully
+ */
+@SpringBootApplication
+public class AplicacaoWeb {
+
+    public static void main(String... args) {
+        SpringApplication.run(AplicacaoWeb.class, args);
+    }
+
+}
